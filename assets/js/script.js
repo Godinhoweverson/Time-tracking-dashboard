5be268f70ffd72b2488f0941ea23e9993fb97f7c
Weverson Godinho <weverson.godinho@icloud.com>
2024-08-30 13:25:44 +0000

    Added javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
+const daily = document.getElementById('daily')
+const weekly = document.getElementById('weekly')
+const monthly = document.getElementById('monthly')
+const time = [...document.querySelectorAll('.time > p')]
+const previous = [...document.querySelectorAll('.last-update > span')]
+
+// Declare variables to store the time tracking data and to check if a timeframe has been selected
 let timeTracking 
+let timeframes = false 
 
-
+// Get the Json file
 fetch('/data.json').then((request) =>{
     if(!request.ok){
         console.log('Oops! Something went wrong.')
@@ -8,19 +16,97 @@ fetch('/data.json').then((request) =>{
     }
     return request.json()
 }).then((data) => {
-    timeTracking = data
-    handleData()
+    timeTracking = data// Store the data in timeTracking
+    handleData() // Call the function to handle the data
 })
-let data = [];
-const handleData = function  (){
- 
-    timeTracking.forEach(element => {
-        data.push(timeTracking[i])
-    });
-       
-    
-    
+
+
+
+//Initial Display
+const displayData = function (){
+    weekly.style.color = 'white'
+    time.map((t , i) =>{
+        t.innerHTML = `${timeTracking[i]. timeframes.weekly.current}hrs`
+    })
+
+    previous.map((p , i) =>{
+        p.innerHTML = `Last Week - ${timeTracking[i]. timeframes.weekly.previous}hrs`
+    })
 }
 
-console.log(data.length)
+
+
+const dailyData = daily.addEventListener('click', () => {
+    
+timeframes = true
+
+    // Update the time elements with the weekly current hours data
+    time.map((t , i) =>{
+        t.innerHTML = `${timeTracking[i]. timeframes.daily.current}hrs`
+    })
+
+    // Update the previous elements with the weekly previous hours data
+    previous.map((p , i) =>{
+        p.innerHTML = `Last Week - ${timeTracking[i]. timeframes.daily.previous}hrs`
+    })
+   
+    if(timeframes){
+        // Change button colors to indicate the active selection
+        daily.style.color = 'white'
+        weekly.style.color = 'hsl(235, 45%, 62%)'
+        monthly.style.color = 'hsl(235, 45%, 62%)'
+    }
+
+    
+})
+
+const WeeklylData = weekly.addEventListener('click', () => {
+    
+    timeframes = true
+        
+    // Update the time elements with the weekly current hours data
+    time.map((t , i) =>{
+        t.innerHTML = `${timeTracking[i]. timeframes.weekly.current}hrs`
+    })
+
+    // Update the previous elements with the weekly previous hours data
+    previous.map((p , i) =>{
+        p.innerHTML = `Last Week - ${timeTracking[i]. timeframes.weekly.previous}hrs`
+    })
+ 
+    if(timeframes){
+        // Change button colors to indicate the active selection
+        weekly.style.color = 'white'
+        daily.style.color = 'hsl(235, 45%, 62%)'
+        monthly.style.color = 'hsl(235, 45%, 62%)'
+    }
+})
+
+const monthlyData = monthly.addEventListener('click', () => {
+    
+    timeframes = true
+
+    // Update the time elements with the weekly current hours data
+    time.map((t , i) =>{
+        t.innerHTML = `${timeTracking[i]. timeframes.monthly.current}hrs`
+    })
+
+     // Update the previous elements with the weekly previous hours data
+    previous.map((p , i) =>{
+        p.innerHTML = `Last Week - ${timeTracking[i]. timeframes.monthly.previous}hrs`
+    })
+    
+    if(timeframes){
+        // Change button colors to indicate the active selection
+        monthly.style.color = 'white'
+        daily.style.color = 'hsl(235, 45%, 62%)'
+        weekly.style.color = 'hsl(235, 45%, 62%)'
+    }
+})
+
+// Function to handle the data once it's fetched and display the initial data
+const handleData = function  (){
+    displayData()// Call the displayData function to set the initial view
+}
+
 
